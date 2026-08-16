38ab7eeb96866d7806c5446dd89b8b78a656fcb5
dh1-Omarchy <237053497+Samz6293@users.noreply.github.com>
2026-08-16 22:45:18 +0600

    Add notificationSystem practice-1

---
+++ typescript-practice/practice-1/safeProcessor.ts
@@ -0,0 +1,28 @@
+function safeProcessor(value: unknown): string | number {
+    if (typeof value === "string") {
+        return `
+        The value "${value}" is string
+        Uppercase: ${value.toUpperCase()}
+        `;
+    }
+    else if(typeof value === "number") {
+        return `
+        The value "${value}" is number
+        Square: ${value * value}
+        `;
+    }
+    else if (Array.isArray(value)) {
+        return `
+        The value "${value}" is an array
+        Total elements in the array: ${value.length}
+        `;
+    }
+    else {
+        return `The value "${value}" is not a string, number or array`;
+    }
+}
+
+console.log(safeProcessor("Hello, World!"));
+console.log(safeProcessor(45));
+console.log(safeProcessor([23,12,12,435,7,8,9]));
+console.log(safeProcessor({name: "motin", joke: 123}));
---
+++ typescript-practice/practice-1/student.js
@@ -0,0 +1,66 @@
+"use strict";
+;
+function averageMarks(student) {
+    const [studentName, studentMarks] = Object.values(student);
+    const averageMarks = studentMarks
+        .reduce((acc, marks) => acc + marks, 0)
+        / studentMarks.length;
+    return `Average marks of ${studentName} is: ${averageMarks}`;
+}
+function averageMarkCount(student) {
+    const [, studentMarks] = Object.values(student);
+    const averageMarks = studentMarks
+        .reduce((acc, marks) => acc + marks, 0)
+        / studentMarks.length;
+    return averageMarks;
+}
+function passedStudents(students) {
+    let passedStudents = [];
+    for (let student of students) {
+        const averageMarks = student.marks
+            .reduce((acc, marks) => acc + marks, 0)
+            / student.marks.length;
+        if (averageMarks >= 40) {
+            passedStudents.push(student);
+        }
+    }
+    return passedStudents;
+}
+function highestAverage(students) {
+    return students.reduce((acc, student) => {
+        if (averageMarkCount(student) > averageMarkCount(acc)) {
+            return student;
+        }
+        return acc;
+    }, students[0]);
+}
+const students = [
+    {
+        name: "Rahim",
+        marks: [78, 85, 92, 74, 88]
+    },
+    {
+        name: "Karim",
+        marks: [45, 52, 39, 61, 48]
+    },
+    {
+        name: "Nadia",
+        marks: [91, 87, 95, 89, 93]
+    },
+    {
+        name: "Sadia",
+        marks: [32, 41, 38, 45, 36]
+    },
+    {
+        name: "Tanvir",
+        marks: [65, 72, 58, 81, 69]
+    },
+    {
+        name: "Farhan",
+        marks: [28, 35, 42, 31, 39]
+    }
+];
+console.log(averageMarks(students[0]));
+console.log(averageMarks(students[3]));
+console.log("Passed students: ", passedStudents(students));
+console.log(highestAverage(students));
